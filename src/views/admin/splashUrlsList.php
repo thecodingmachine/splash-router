1ef4fa90cae1dc30a5ebe5d8dddbacfcd192bb2e
David Négrier <d.negrier@thecodingmachine.com>
2013-03-21 20:30:21 +0100

    Fixing URL to instance in UI

--- src/views/admin/splashUrlsList.php
+++ src/views/admin/splashUrlsList.php
@@ -17,7 +17,7 @@
 	?>
 	<tr class="<?php echo (($i%2)?"odd":"even") ?>">
 		<td title="<?php echo plainstring_to_htmlprotected($splashUrl->comment); ?>"><?php echo "/".$splashUrl->url ?></td>
-		<td><?php echo '<a href="'.ROOT_URL.'mouf/instance/?name='.plainstring_to_htmlprotected($splashUrl->controllerInstanceName).'&selfedit='.$this->selfedit.'">'.$splashUrl->controllerInstanceName.'</a>'; ?></td>
+		<td><?php echo '<a href="'.ROOT_URL.'mouf/ajaxinstance/?name='.plainstring_to_htmlprotected($splashUrl->controllerInstanceName).'&selfedit='.$this->selfedit.'">'.$splashUrl->controllerInstanceName.'</a>'; ?></td>
 		<td><?php echo plainstring_to_htmlprotected($splashUrl->title) ?></td>
 		<td><?php echo $splashUrl->methodName ?></td>
 	</tr>
