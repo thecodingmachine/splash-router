3423d04bb31ef7d4e667106f064195c22bf47197
David Négrier <d.negrier@thecodingmachine.com>
2013-08-08 18:26:08 +0200

    Adding URLEntryPoint

---
+++ src/Mouf/Mvc/Splash/UrlEntryPoint.php
@@ -0,0 +1,58 @@
+<?php
+namespace Mouf\Mvc\Splash;
+
+use Mouf\Mvc\Splash\Controllers\Controller;
+
+use Mouf\Utils\Action\ActionInterface;
+
+use Mouf\MoufManager;
+
+use Mouf\Mvc\Splash\Services\SplashRoute;
+
+use Mouf\Mvc\Splash\Services\UrlProviderInterface;
+
+
+/**
+ * This class represents a single URL that can be bound to a specific behaviour.
+ * 
+ * FIXME: we should not have to extend Controller. Modify Splash to remove this limitation.
+ * 
+ * @author David Negrier
+ */
+class UrlEntryPoint extends Controller implements UrlProviderInterface {
+	
+	private $url;
+	private $actions;
+	
+	/**
+	 * Construct the object passing in parameter the URL and the list of actions to perform
+	 * 
+	 * @param string $url The URL to bind to. It should
+	 * @param array<ActionInterface> $actions
+	 */
+	public function __construct($url, array $actions = array()) {
+		$this->url = $url;
+		$this->actions = $actions;
+	}
+	
+	/**
+	 * Method called when the URL is called.
+	 */
+	public function action() {
+		foreach ($this->actions as $action) {
+			$action->run();
+		}
+	}
+	
+	/**
+	 * Returns the list of URLs that can be accessed, and the function/method that should be called when the URL is called.
+	 *
+	 * @return array<SplashRoute>
+	 */
+	function getUrlsList() {
+		$instanceName = MoufManager::getMoufManager()->findInstanceName($this);
+		
+		$route = new SplashRoute($this->url, $instanceName, "action", null, null);
+		return array($route);
+	}
+}
