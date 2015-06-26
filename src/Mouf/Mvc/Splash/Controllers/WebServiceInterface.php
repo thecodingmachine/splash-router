5540734a0e20b99db69614846134d35ec7a574a9
David Négrier <d.negrier@thecodingmachine.com>
2015-06-26 11:46:47 +0200

    Moving webservice class from splash to splash-common

---
+++ src/Mouf/Mvc/Splash/Controllers/WebServiceInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace Mouf\Mvc\Splash\Controllers;
+
+interface WebServiceInterface
+{
+
+    /**
+     * Returns the URI of the Webservice
+     * @return string
+     */
+    public function getWebserviceUri();
+}
